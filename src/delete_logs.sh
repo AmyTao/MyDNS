443736e7f70280e7b079cbfc266963b6810a1bc5
future-ubuntu2204 <3038242641@qq.com>
2024-05-17 13:30:53 +0800

    Updated delete_logs.sh

--- src/delete_logs.sh
+++ src/delete_logs.sh
@@ -1,28 +1,28 @@
 if [ -f "kvInfo.log" ]; then
     rm "kvInfo.log"
-    echo "kvInfo.log 已删除"
+    echo "kvInfo.log Deleted"
 else
-    echo "kvInfo.log 不存在"
+    echo "kvInfo.log not exist"
 fi
 
 if [ -f "shardInfo.log" ]; then
     rm "shardInfo.log"
-    echo "shardInfo.log 已删除"
+    echo "shardInfo.log Deleted"
 else
-    echo "shardInfo.log 不存在"
+    echo "shardInfo.log not exist"
 fi
 
 
 if [ -f "raftInfo.log" ]; then
     rm "raftInfo.log"
-    echo "raftInfo.log 已删除"
+    echo "raftInfo.log Deleted"
 else
-    echo "raftInfo.log 不存在"
+    echo "raftInfo.log not exist"
 fi
 
 if [ -f "raftWarn.log" ]; then
     rm "raftWarn.log"
-    echo "raftWarn.log 已删除"
+    echo "raftWarn.log Deleted"
 else
-    echo "raftWarn.log 不存在"
+    echo "raftWarn.log not exist"
 fi
